dd4278ad3d3accd772f57573c189702ab872de95
Ivan Nazarov <ivan.nazarov@nymcard.com>
2025-04-11 10:23:31 +0300

    feat: Update iOS project settings and dependencies
    - Updated Runner.xcscheme to reflect new LastUpgradeVersion and added testable references.
    - Changed AppDelegate.swift to use @main instead of @UIApplicationMain.
    - Updated app icons in Assets.xcassets with new images.
    - Modified Info.plist to include CFBundleDisplayName and additional keys for indirect input events.
    - Added a new test file RunnerTests.swift with a placeholder test case.
    - Refactored main.dart to simplify widget structure.
    - Updated pubspec.yaml and pubspec.lock to reflect new dependencies and versions, including flutter_lints and updated SDK constraints.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -114,13 +114,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('You have pushed the button this many times:'),
+            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),
